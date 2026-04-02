89a3d6b258fe38d12579657aee173a65a58297e9
sahildhatwalia <sahildhatwalia10@gmail.com>
2026-04-02 21:21:08 +0530

    main

--- server/server.js
+++ server/server.js
@@ -11,7 +11,7 @@ const app = express();
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175'],
+  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:5175', 'http://127.0.0.1:5173'],
   credentials: true
 }));
 
